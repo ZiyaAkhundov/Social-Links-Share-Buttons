5385b4599a133dd0262fcf5b8f1391f360a7dd7d
Ziya Akhundov <95044753+ZiyaAkhundov@users.noreply.github.com>
2023-04-04 12:29:37 +0400

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,32 +1,62 @@
 const selecttedTextArea = document.querySelector(".selected-text-area");
-const twitterBtn=document.querySelector("#twitterPublish");
+const twitterBtn = document.querySelector("#twitterPublish");
+const socialLinks = document.querySelector(".socialLinks");
+const whatsappBtn = document.querySelector("#whatsappPublish");
+const telegramBtn = document.querySelector("#telegramPublish");
 
 selecttedTextArea.addEventListener("mouseup", selectedTextAreaOnMouseUp);
-function selectedTextAreaOnMouseUp(e){
+function selectedTextAreaOnMouseUp(e) {
     setTimeout(() => {
         const selectedText = window.getSelection().toString().trim();
-    if(selectedText.length){
-        twitterBtn.style.left = `${e.pageX + Number(getComputedStyle(twitterBtn).width.slice(0,-5))}px`;
-        twitterBtn.style.top = `${e.pageY - Number(getComputedStyle(twitterBtn).height.slice(0,-2))}px`;  
-        twitterBtn.style.display="block";
-        twitterBtn.classList.add("twitterBtnAnimation");
-    }
+        if (selectedText.length) {
+            socialLinks.style.left = `${e.pageX + Number(getComputedStyle(twitterBtn).width.slice(0, -5))}px`;
+            socialLinks.style.top = `${e.pageY - Number(getComputedStyle(twitterBtn).height.slice(0, -2))}px`;
+            socialLinks.style.display = "inline";
+            twitterBtn.classList.add("BtnAnimation");
+            whatsappBtn.classList.add("BtnAnimation");
+            telegramBtn.classList.add("BtnAnimation");
+            console.log(getComputedStyle(socialLinks).display)
+        }
     }, 0)
 }
 
-    document.addEventListener("mousedown",(event) =>{
-        if(getComputedStyle(twitterBtn).display === "block" && event.target.id !== "twitterPublish"){
-            twitterBtn.style.display = "none";
-            twitterBtn.classList.remove("twitterBtnAnimation");
-            window.getSelection().empty();
-        }
-    });
-
-    twitterBtn.addEventListener("click",twitterShareBtnClick);
-    function twitterShareBtnClick(){
-        const twitterShareLink = 'https://twitter.com/intent/tweet';
-        const text = encodeURIComponent(window.getSelection().toString().trim());
-        const hashtags ="programming, JavaScript";
-        const via= 'ProgrammersSchool';
-        window.open(`${twitterShareLink}?text="${text}" by @${via} ${hashtags.split(", ").map(h => "%23" +h).join(" ")} ${encodeURIComponent(window.location.href)}`);
+document.addEventListener("mousedown", (event) => {
+    if (getComputedStyle(socialLinks).display === "block" && !isClickInsideElement(event.target, socialLinks)) {
+        socialLinks.style.display = "none";
+        twitterBtn.classList.remove("BtnAnimation");
+        whatsappBtn.classList.remove("BtnAnimation");
+        telegramBtn.classList.remove("BtnAnimation");
+        window.getSelection().empty();
     }
+});
+function isClickInsideElement(clickedElement, parentElement) {
+    if (clickedElement === parentElement) {
+        return true;
+    } else if (clickedElement.parentNode) {
+        return isClickInsideElement(clickedElement.parentNode, parentElement);
+    } else {
+        return false;
+    }
+}
+
+twitterBtn.addEventListener("click", twitterShareBtnClick);
+whatsappBtn.addEventListener("click", whatsappShareClick);
+telegramBtn.addEventListener("click", telegramShareBtnClick);
+
+function twitterShareBtnClick() {
+    const twitterShareLink = 'https://twitter.com/intent/tweet';
+    const hashtags = "programming, JavaScript";
+    const text = encodeURIComponent(window.getSelection().toString().trim());
+    const via = 'ProgrammersSchool';
+    window.open(`${twitterShareLink}?text="${text}" by @${via} ${hashtags.split(", ").map(h => "%23" + h).join(" ")} ${encodeURIComponent(window.location.href)}`);
+}
+
+function whatsappShareClick() {
+    const text = encodeURIComponent(window.getSelection().toString().trim());
+    window.open(`https://api.whatsapp.com/send?text=${text + " shared by"}%20${encodeURIComponent(window.location.href)}`);
+}
+
+function telegramShareBtnClick() {
+    const text = encodeURIComponent(window.getSelection().toString().trim());
+    window.open(`https://telegram.me/share/url?url=${encodeURIComponent(window.location.href)}&text=${text}`);
+}
